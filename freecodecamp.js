ae349157284385097906d51c43d6ad54df5d31ca
SyedSihabUsSakib <sakibshihab12@gmail.com>
2022-07-30 23:44:26 +0600

    first commit

---
+++ freecodecamp.js
@@ -0,0 +1,12 @@
+class Circle {
+    #radius;
+    constructor(value) {
+      this.#radius = value;
+    }
+    get area() {
+      return Math.PI * Math.pow(this.#radius, 2);
+    }
+  }
+  
+  let circle = new Circle(10);
+  console.log(circle.area); // 314.1592653589793
