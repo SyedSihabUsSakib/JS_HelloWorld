5d17ed6228854aeefa436d984566a83cde83e788
SyedSihabUsSakib <sakibshihab12@gmail.com>
2022-08-16 01:50:32 +0600

    TODO App

---
+++ ToDoApp_DOM/script.js
@@ -0,0 +1,44 @@
+// console.log(document.all);
+
+// for(let element of document.all)
+// {
+//     console.log(element);
+// }
+
+document.title="Task Manager with DOM";
+// console.log(typeof(document));
+// console.log(document.head);
+
+// let el=document.images;
+// for(let element of el)
+// {
+//     console.log(element);
+// }
+
+
+// Iterable
+
+// const arr = ['hi','hello','all'];
+// // console.dir(arr);
+
+// let it=arr[Symbol.iterator]();
+// console.log(it);
+// // onsole.log(it.next());
+// it.next()
+// it.next()
+// console.log(it.next());
+
+
+// const obj={
+//     robin: 12,
+//     sihab: "ninety"
+// };
+// // for(let element of obj)
+// // {
+// //     console.log(element);
+// // }
+
+// console.dir(obj);
+
+// Traversing DOm
+
